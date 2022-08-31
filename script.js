f46d4df196eb17f981b9db1d731aacda0ffd5578
eliassbaranovs <82933682+eliassbaranovs@users.noreply.github.com>
2022-08-31 15:06:41 +0300

    Continuing with modules

--- script.js
+++ script.js
@@ -4,6 +4,48 @@
 // addToCart('Bread', 2);
 // console.log(price, qt);
 //Imports everything that is exported from shoppingCart.js
+/*
 import * as shoppingCart from './shoppingCart.js';
 shoppingCart.addToCart('Bread', 2);
 console.log(shoppingCart.totalPrice, shoppingCart.qt);
+*/
+/*
+//Import default export, can name the function as you want
+import add from './shoppingCart.js';
+add('Bread', 5);
+add('Pizza', 11);
+add('Apples', 3);
+
+//Imports are not copies, they are live-connected
+import { cart } from './shoppingCart.js';
+console.log(cart);
+/*
+//Top level await, but this block execution of the rest of the code/module
+console.log('Start fethcing');
+const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+const data = await res.json();
+console.log(data);
+console.log('End of fetching, because fetch blocks me from running');
+
+
+//Async function doesnt block the rest of the code, it is executed in the background
+console.log('1');
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  //It will return a promise, not the object, since its still awaiting information
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+console.log('2');
+// const lastPost = getLastPost(); //Returns promise
+// console.log(lastPost); // Returns promise
+// lastPost.then(last => console.log(last)); //Not very clean
+
+const lastPost2 = await getLastPost(); //await waits for the promise to be resolved
+console.log(lastPost2); //Returns object
+*/
+
+/*
+//CommonJS import, works only in nodejs
+const {addToCart } = require('./shoppingCart.js');
+*/
--- shoppingCart.js
+++ shoppingCart.js
@@ -1,7 +1,14 @@
 //Exporting module
 
 const shippingCost = 10;
-const cart = [];
+export const cart = [];
+//Top level await blocking the rest of the code before it finishes
+//Blocking code (blocks the rest of the code from running in script.js before it  is finished)
+/*
+console.log('Start fethcing');
+await fetch('https://jsonplaceholder.typicode.com/users');
+console.log('Finished fetching data');
+*/
 
 export const addToCart = function (product, quantity) {
   cart.push({
@@ -13,5 +20,25 @@ export const addToCart = function (product, quantity) {
 
 const totalPrice = 237;
 const totalQuantity = 23;
-export { totalPrice, totalQuantity as qt };
 // qt = totalQuantity, can change variable name to whatever we want
+export { totalPrice, totalQuantity as qt };
+
+//Default exports(exports a value)
+export default function (product, quantity) {
+  cart.push({
+    product: product,
+    quantity: quantity,
+  });
+  console.log(`${quantity} ${product} has been added to the cart`);
+}
+
+//Common JS modules works only in node.js
+/*
+export.addToCart =  function (product, quantity) {
+  cart.push({
+    product: product,
+    quantity: quantity,
+  });
+  console.log(`${quantity} ${product} has been added to the cart`);
+};
+*/
